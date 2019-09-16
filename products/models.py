cba3d53f3746e7a9a1414d0f37fdfec3936d0773
Senthil <DSENTHI5@ford.com>
2019-09-16 17:14:04 +0530

    updated with Addtocart

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
-
+from django.contrib.auth.models import User
+DATE_INPUT_FORMATS = ['%d-%m-%Y']
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
@@ -12,6 +12,13 @@ class Offer(models.Model):
     code = models.CharField(max_length=255)
     description = models.CharField(max_length=2018)
     discount = models.FloatField()
+
+
+class Addtocart(models.Model):
+    code = models.ForeignKey(Offer,on_delete=models.CASCADE)
+    name = models.ForeignKey(Product,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    dateoforder = models.DateField(null=True)
 
 
 
